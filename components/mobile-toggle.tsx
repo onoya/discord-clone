55646cd651156754abda96f2ee313a8d6d5aa0ad
Naoya Ono <ono.naoyaa@gmail.com>
2023-08-28 14:42:19 +0800

    add mobile toggle

---
+++ components/mobile-toggle.tsx
@@ -0,0 +1,26 @@
+import NavigationSidebar from '@/components/navigation/navigation-sidebar';
+import ServerSidebar from '@/components/server/server-sidebar';
+import { Button } from '@/components/ui/button';
+import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { MenuIcon } from 'lucide-react';
+
+const MobileToggle = ({ serverId }: { serverId: string }) => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <MenuIcon />
+        </Button>
+      </SheetTrigger>
+
+      <SheetContent side="left" className="p-0 flex gap-0">
+        <div className="w-[72px]">
+          <NavigationSidebar />
+        </div>
+        <ServerSidebar serverId={serverId} />
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileToggle;
