9179de2050872d88f7c2ca8ec37800a38e744a87
Naoya Ono <ono.naoyaa@gmail.com>
2023-08-28 14:12:25 +0800

    edit channel functionality

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,6 +4,7 @@ import { CreateChannelModal } from '@/components/modals/create-channel-modal';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { DeleteChannelModal } from '@/components/modals/delete-channel-modal';
 import { DeleteServerModal } from '@/components/modals/delete-server-modal';
+import { EditChannelModal } from '@/components/modals/edit-channel-modal';
 import { EditServerModal } from '@/components/modals/edit-server-modal';
 import { InviteModal } from '@/components/modals/invite-modal';
 import { LeaveServerModal } from '@/components/modals/leave-server-modal';
@@ -29,6 +30,7 @@ export const ModalProvider = () => {
       <LeaveServerModal />
       <DeleteServerModal />
       <DeleteChannelModal />
+      <EditChannelModal />
     </>
   );
 };
